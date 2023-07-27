513ae2cd2d1e5a94a57d2eaf5dac0a619cd8bba4
VTRAPZENIKOV\Vova <vtrapeznikov1993@gmail.com>
2023-07-27 19:29:21 +0300

    href

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -6,7 +6,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-    @ViewChild('max') max: HTMLImageElement;
     public isMaxHidden = false;
     public isFrogHidden = true;
 
